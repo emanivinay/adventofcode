1e55c45e123f749fae66450095359cb0550242b2
Vinay Emani <vinnymaker18@gmail.com>
2022-12-14 10:13:32 +0530

    Python template code additions

--- 2022/common/program_template.py
+++ 2022/common/program_template.py
@@ -1,4 +1,13 @@
 import sys
+import collections
+import itertools
+import functools
+
+def split_line_to_type(line, type_converter):
+    return [type_converter(word) for word in line.split()]
+
+
+split_ints = lambda line: split_line_to_type(line, int)
 
 
 def main():
